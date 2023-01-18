5b225e9c59609235b0e9a014ba313c1b6430733b
Francis Ofori Anane <ofori.anane@gmail.com>
2023-01-18 02:28:27 +0000

    Add commit

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i < 10:
-        print("{0}{1},".format(0, i), end=" ")
-    elif i > 9 and i < 99:
-        print("{0},".format(i), end=" ")
+    if i < 99:
+        print("{:02d},".format(i), end=" ")
     elif i == 99:
-        print(i)
+        print("{0}".format(i))
