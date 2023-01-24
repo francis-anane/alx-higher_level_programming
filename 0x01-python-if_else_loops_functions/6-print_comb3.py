db1044de2a85f4e0c2b2685a7bed1b4d27843845
Francis Ofori Anane <ofori.anane@gmail.com>
2023-01-24 16:12:20 +0000

    Add commit

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for i in range(48, 58):
+    for j in range(48, 58):
+        if i != j and i < j:
+            print("{0}{1}".format(chr(i), chr(j)), end="")
+            if j == 57 and i == 56:
+                break
+            print(",".format(), end=" ")
+print("\n".format())
