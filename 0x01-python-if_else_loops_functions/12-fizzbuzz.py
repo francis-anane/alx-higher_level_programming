fe63512821e428f2e7bfc4aa4427219fdb8a9c33
Francis Ofori Anane <ofori.anane@gmail.com>
2023-01-25 14:48:57 +0000

    Update commit

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,10 @@
+def fizzbuzz():
+    for n in range(1, 101):
+        if (n % 3 == 0) and (n % 5 != 0):
+            print("Fizz", end=" ")
+        elif (n % 5 == 0) and (n % 3 != 0):
+            print("Buzz", end=" ")
+        elif (n % 3 == 0) and (n % 5 == 0):
+            print("FizzBuzz", end=" ")
+        else:
+            print(n, end=" ")
