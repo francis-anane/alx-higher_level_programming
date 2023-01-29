dcb73d77c7e8c83053049c96ba97fe4c2b31ad18
Francis Ofori Anane <ofori.anane@gmail.com>
2023-01-29 23:20:56 +0000

    Add commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-   from sys import argv
+    from sys import argv
 
-   ac = len(argv)
-   if ac < 2:
-      print("0 arguments.")
-   elif ac == 2:
-      print("1 argument:")
-      print("1: {0}".format(argv[1]))
-   elif ac > 2:
-      print("{0} arguments:".format(ac-1))
-      i = 1
-      for a in argv:
-         if i <= ac - 1:
-            print("{0}: {1}".format(i, argv[i]))
-            i = i + 1
+    ac = len(argv)
+    if ac < 2:
+        print("0 arguments.")
+    elif ac == 2:
+        print("1 argument:")
+        print("1: {0}".format(argv[1]))
+    elif ac > 2:
+        print("{0} arguments:".format(ac-1))
+        i = 1
+        for a in argv:
+            if i <= ac - 1:
+                print("{0}: {1}".format(i, argv[i]))
+                i = i + 1
