35cb5faf617d83081180afc682216d4e5bf02f6a
Francis Ofori Anane <ofori.anane@gmail.com>
2023-01-29 23:26:50 +0000

    Add commit

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,11 +3,11 @@ if __name__ == "__main__":
     from sys import argv
 
     ac = len(argv)
+    num = 0
     if ac > 1:
         i = 1
-        num = 0
         for a in argv:
             if i <= ac - 1:
                 num = num + int(argv[i])
                 i = i + 1
-        print(num)
+    print(num)
