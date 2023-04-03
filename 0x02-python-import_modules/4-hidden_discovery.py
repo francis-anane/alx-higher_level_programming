d66d442bc99aa5b413706e8edbad34f354b2a7bf
Francis Ofori Anane <ofori.anane@gmail.com>
2023-04-03 15:28:25 +0000

    add 4-hidden_discovery.py 5-variable_load.py

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+
+    import hidden_4
+
+    names = dir(hidden_4)
+
+    names = list(names)
+
+    for name in names:
+        if name[0] != "_":
+            print(name)
