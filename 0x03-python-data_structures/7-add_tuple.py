796388177e9a1bf9f01f0c426e5d81a7c7d04fad
Francis Ofori Anane <ofori.anane@gmail.com>
2023-04-06 13:34:29 +0000

    Update 7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -6,7 +6,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
 
     ta_len = len(tuple_a)
     tb_len = len(tuple_b)
-    tupl = tuple_a, tuple_b
+    tupl = 0, 0
     if (not ta_len >= 2) or (not tb_len >= 2):
         if (((ta_len < 2) and (tb_len < 2)) and (ta_len != 0 and tb_len != 0)):
             tupl = tuple_a[0] + tuple_b[0], 0
