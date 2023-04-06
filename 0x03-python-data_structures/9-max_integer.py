3106b016ca1b66401679bffdca0f9709dee6f997
Francis Ofori Anane <ofori.anane@gmail.com>
2023-04-06 13:49:55 +0000

    Update 9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -9,7 +9,7 @@ def max_integer(my_list=[]):
     if llen == 0:
         return (None)
     else:
-        max_int = 0
+        max_int = my_list[0]
         for i in my_list:
             if i > max_int:
                 max_int = i
