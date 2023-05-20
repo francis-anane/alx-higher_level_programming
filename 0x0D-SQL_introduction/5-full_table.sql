d34eeb78064910b5cd8ea49257db3bc1e59624ca
Francis Ofori Anane <ofori.anane@gmail.com>
2023-05-20 13:53:31 +0000

    Added project files

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,5 @@
+-- This script shows the full description of the table first_table
+-- in the database hbtn_0c_0 passed as
+-- a commandline argument in MySQL server
+
+SHOW CREATE TABLE first_table
