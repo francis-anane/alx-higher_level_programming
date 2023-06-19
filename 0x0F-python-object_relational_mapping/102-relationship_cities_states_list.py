78d79315d5fa5337d5a647e9c067b44ecb3010fb
Francis Ofori Anane <ofori.anane@gmail.com>
2023-06-19 17:45:06 +0000

    Add files

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""lists all City objects from the database hbtn_0e_101_usa
+"""
+from sys import argv
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship
+from sqlalchemy import asc
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for state in session.query(State).order_by(asc(State.id)):
+        for city in state.cities:
+            print(city.id, city.name, sep=": ", end="")
+            print(" -> " + city.name)
