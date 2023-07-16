0f4773e4913553f797da6812c5c0aaf463126f89
Francis Ofori Anane <ofori.anane@gmail.com>
2023-07-16 17:58:54 +0000

    Update file

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Sends a JSON POST request to a given URL passed as first argument and
-# with a given JSON file passed as second argument.
+# Sends a JSON POST request to a given URL passed as first argument and with a given JSON file passed as second argument.
 curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
