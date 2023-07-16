c1fe0d3ed935eba637acd205ff82fef14f04bb22
Francis Ofori Anane <ofori.anane@gmail.com>
2023-07-16 18:09:51 +0000

    Update file

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Makes a request to 0.0.0.0:5000/catch_me that makes the server respond with the message "You got me!".
-curl -sL -X PUT  0.0.0.0:5000/catch_me
+curl -sL -X PUT -d "user_id=98" 0.0.0.0:5000/catch_me
