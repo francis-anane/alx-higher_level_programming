9e6c435f2cc634dcdd700322ac22ecc9e8948a62
Francis Ofori Anane <ofori.anane@gmail.com>
2023-07-16 18:15:17 +0000

    Update file

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Sends a GET request to a given URL passed as argument to the script
-# with a header variable, X-School-User-Id with the value 98.
+# Sends a GET request to a given URL passed as argument to the script with a header variable, X-School-User-Id with the value 98.
 curl -sH "X-School-User-Id: 98" "${1}"
