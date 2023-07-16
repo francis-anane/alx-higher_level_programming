a15695c3b2aca704c13b3ff6b7dd1f3da5c71e21
Francis Ofori Anane <ofori.anane@gmail.com>
2023-07-16 17:38:09 +0000

    Update file

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This script gets the byte size of the HTTP response
-# header for a given URL passed as argument.
+# This script gets the byte size of the HTTP responseheader for a given URL passed as argument.
 curl -s "$1" | wc -c
