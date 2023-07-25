a831616dcf812b2988c725e1e12bb5f78c4caa08
Francis Ofori Anane <ofori.anane@gmail.com>
2023-07-25 13:56:52 -0400

    Update file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,15 +1,16 @@
 #!/usr/bin/node
-/*Reads and prints the content of a file passed as CLI argument.*/
 
-const fs = require("fs"); // import file system
+/* Reads and prints the content of a file passed as CLI argument. */
 
-if (process.argv.length == 3){
-    //The readFile method takes filename, encoding, and callback function as parameter
-    let content = fs.readFile(process.argv[2], "utf-8", (err, data) => {
-	if (err){
-	    console.log(err);
-	} else {
-	    console.log(data);
-	}
-    });
+const fs = require('fs'); // import file system
+
+if (process.argv.length === 3) {
+  /* The readFile method takes filename, encoding, and callback function as parameter */
+  fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data);
+    }
+  });
 }
